6ef271cd73f56d9f5c1db371f021c66c2a7e6e0f
Daniyar Idrisov <dan.id2001@mail.ru>
2022-12-20 00:06:56 +0300

    added new dbScan.py

--- programms/dbScan.py
+++ programms/dbScan.py
@@ -1,97 +1,81 @@
 import pygame
 import numpy as np
 
-colors = ["#ffe4e1", "#eac93c", "#0d324d", "#637dac", "#242533", "#724ea2", "#b6e2ad", "#bce9f1", "#a981a9"]
+MIN = 6
+EPS = 60
+COLORS = ["#f08077", "#bce9f1", "#5a3036", "#637dac", "#eac93c", "#b6e2ad", "#5c8910", "#724ea2"]
 
 
-def dist(point1, point2):
-    return np.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
-
-
-def neighbours(point1, points, groups, flags, e, g):
-    for i, pnt2 in enumerate(points):
-        if groups[i] == 0 and dist(point1, pnt2) < e:
+def neighbors(p1, points, groups, f, g):
+    for i, p2 in enumerate(points):
+        if groups[i] == 0 and dist(p1, p2) < EPS:
             groups[i] = g
-            if flags[i] != 'y':
-                neighbours(pnt2, points, groups, flags, e, g)
+            if f[i] != 'y':
+                neighbors(p2, points, groups, f, g)
+
+
+def dist(p1, p2):
+    return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
 
 def dbscan(points):
-    min_points = 3
-    eps = 60
-
     f = ['r' for _ in range(len(points))]
-
-    for i, point1 in enumerate(points):
-        number_of_points = 0
-
-        for point2 in points:
-            if point1 != point2 and dist(point1, point2) < eps:
-                number_of_points += 1
-
-        if number_of_points >= min_points:
+    for i, p1 in enumerate(points):
+        n = 0
+        for p2 in points:
+            if p1 != p2 and dist(p1, p2) < EPS:
+                n += 1
+        if n >= MIN:
             f[i] = 'g'
-
-    for i, point1 in enumerate(points):
+    for i, p1 in enumerate(points):
         if f[i] != 'g':
-            for j, point2 in enumerate(points):
-                if f[j] == 'g' and point1 != point2 and dist(point1, point2) < eps:
+            for j, p2 in enumerate(points):
+                if f[j] == 'g' and p1 != p2 and dist(p1, p2) < EPS:
                     f[i] = 'y'
                     break
-
     groups = [0 for _ in range(len(points))]
-
     g = 0
-    for i, point1 in enumerate(points):
+    for i, p1 in enumerate(points):
         if f[i] == 'g' and groups[i] == 0:
             g += 1
-            neighbours(point1, points, groups, f, eps, g)
-
+            neighbors(p1, points, groups, f, g)
     return f, groups
 
 
-def start():
+if __name__ == 'main':
     pygame.init()
-
-    s = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((800, 600))
     running = True
-
-    s.fill("white")
-
+    screen.fill("white")
     pygame.display.update()
-    points = []
-
+    p = []
     while running:
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    points.append(event.p)
-                    pygame.draw.circle(s, color='black', center=event.p, radius=10)
+                    p.append(event.pos)
+                    pygame.draw.circle(screen, color='black', center=event.pos, radius=10)
                     pygame.display.update()
             if event.type == pygame.KEYDOWN:
                 print(event.key)
                 if event.key == pygame.K_1:
-                    flags, groups = dbscan(points)
-                    s.fill("white")
-                    for i, pnt in enumerate(points):
-                        clr = flags[i]
-                        if clr == 'r':
-                            clr = 'red'
-                        elif clr == 'y':
-                            clr = 'yellow'
+                    flags, groups = dbscan(p)
+                    screen.fill("white")
+                    for i, pnt in enumerate(p):
+                        color = flags[i]
+                        if color == 'r':
+                            color = 'red'
+                        elif color == 'y':
+                            color = 'yellow'
                         else:
-                            clr = 'green'
-                        pygame.draw.circle(s, color=clr, center=pnt, radius=10)
+                            color = 'green'
+                        pygame.draw.circle(screen, color=color, center=pnt, radius=10)
                     pygame.display.update()
                 if event.key == pygame.K_2:
-                    flags, groups = dbscan(points)
-                    s.fill('white')
-                    for i, pnt in enumerate(points):
-                        pygame.draw.circle(s, color=colors[groups[i]], center=pnt, radius=10)
+                    flags, groups = dbscan(p)
+                    screen.fill('white')
+                    for i, pnt in enumerate(p):
+                        pygame.draw.circle(screen, color=COLORS[groups[i]], center=pnt, radius=10)
                     pygame.display.update()
             if event.type == pygame.QUIT:
                 running = False
-
-
-if __name__ == '__main__':
-    start()
