06c13503be00f304b93fe7a71bb80c98d339711b
Daniyar Idrisov <dan.id2001@mail.ru>
2022-12-19 21:55:57 +0300

    added neural.py and added new k-means.py

--- programms/k-means.py
+++ programms/k-means.py
@@ -1,91 +1,105 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-N = 78
-x = np.random.randint(1, 1000, N)
-y = np.random.randint(1, 1000, N)
-x_c = np.mean(x)
-y_c = np.mean(y)
-w = []
+J = []
+N = 1000
+MIN = 1000000
 
 
-def draw(x, y, c, xz, yz, k):
-    length = len(c)
-    for i in range(0, length):
-        color = (c[i] + 1) / k
-        plt.scatter(x[i], y[i], color=(color, 0.2, color ** 2))
-    plt.scatter(xz, yz, color='orange')
+def distention(z, p):
+    return np.sqrt((z[0] - p[0]) ** 2 + (z[1] - p[1]) ** 2)
+
+
+def nearest(points, c):
+    cluster = np.zeros(len(points))
+    for i in range(len(points)):
+        min = np.infty
+        for j in range(len(c)):
+            if min > distention(points[i], c[j]):
+                min = distention(points[i], c[j])
+                cluster[i] = j
+    return cluster
+
+
+def plot(points, c, cluster):
+    color_array = ['grey', 'orange', 'pink', 'm', 'k', 'purple', 'b', 'g', 'y', 'c']
+    colors = []
+    x = []
+    y = []
+    for i in range(len(points)):
+        x.append(points[i][0])
+        y.append(points[i][1])
+        colors.append(color_array[int(cluster[i])])
+    x_c = []
+    y_c = []
+    for i in range(len(c)):
+        x_c.append(c[i][0])
+        y_c.append(c[i][1])
+    plt.scatter(x, y, color=colors)
+    plt.scatter(x_c, y_c, color='r')
     plt.show()
 
 
-def dist(x1, y1, x2, y2):
-    return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+def centroids(p, k):
+    x = 0
+    y = 0
+    for i in range(len(p)):
+        x = x + p[i][0]
+        y = y + p[i][1]
+    x = x / len(p)
+    y = y / len(p)
+    r = 0
+    for i in range(len(p)):
+        if r < distention([x, y], p[i]):
+            r = distention([x, y], p[i])
+    c = []
+    for i in range(k):
+        x_c = r * (np.cos(2 * np.pi * i / k)) + x
+        y_c = r * (np.sin(2 * np.pi * i / k)) + y
+        c.append([x_c, y_c])
+    return c
 
 
-def check(x, y, xz, yz, clust, k):
-    x_o, y_o = xz, yz
-    clust(xz, yz, x, y, k, clust)
-    a_clust(x, y, clust, k, xz, yz)
-    draw(x, y, clust, xz, yz, k)
-
-    if x_o == xz and y_o == yz:
-        w = 0
-        for i in range(0, k):
-            sum = 0
-            for j in range(0, len(clust)):
-                if clust[j] == i:
-                    sum += dist(x[j], y[j], xz[i], yz[i]) ** 2
-
-            w += sum
-
-        w.append(w)
-        return True
-    else:
-        return False
-
-
-def clust(xz, yz, x, y, k, cluster):
-    for i in range(0, N):
-        r = dist(xz[0], yz[0], x[i], y[i])
-        f = 0
-        for j in range(0, k):
-            if r < dist(xz[j], yz[j], x[i], y[i]):
-                f = j
-                r = dist(xz[j], yz[j], x[i], y[i])
-            if j == k - 1:
-                cluster[i] = f
-
-
-def a_clust(x, y, c, k, xz, yz):
+def check(p, c, cluster, k):
+    new_cluster = nearest(p, c)
+    plot(p, c, cluster)
+    count = 0
     for i in range(0, k):
-        z_x, z_y = [], []
-        for j in range(0, len(c)):
-            if c[j] == i:
-                z_x.append(x[j])
-                z_y.append(y[j])
-        xz[i] = np.mean(z_x)
-        yz[i] = np.mean(z_y)
+        sum = 0
+        for j in range(0, len(new_cluster)):
+            if new_cluster[j] == i:
+                sum = sum + distention(p[j], c[i]) ** 2
+        count = count + sum
+    J.append(count)
+    return True
 
 
-def k_mean(k):
-    q = 0
-    for i in range(0, N):
-        if dist(x_c, y_c, x[i], y[i]) > q:
-            q = dist(x_c, y_c, x[i], y[i])
-    xzz = [q * np.cos(2 * np.pi * i / k) + x_c for i in range(k)]
-    yzz = [q * np.sin(2 * np.pi * i / k) + y_c for i in range(k)]
-    cluster = [0] * N
-    clust(xzz, yzz, x, y, k, cluster)
-    draw(x, y, cluster, xzz, yzz, k)
-
-    while not check(x, y, xzz, yzz, cluster, k):
-        check(x, y, xzz, yzz, cluster, k)
+def kmeans(k, points):
+    c = centroids(points, k)
+    cluster = nearest(points, c)
+    plot(points, c, cluster)
+    while not check(points, c, cluster, k):
+        check(points, c, cluster, k)
 
 
-if __name__ == "__main__":
+def koptimal_with_kmeans():
+    points = []
+    for i in range(N):
+        points.append(np.random.randint(1, 100, 2))
     for i in range(1, 10):
-        k_mean(i)
-    plt.plot(range(1, 10), w)
-    plt.xlabel('Cluster Num')
-    plt.ylabel('WCSS')
+        kmeans(i, points)
+    global MIN
+    array = []
+    for i in range(1, len(J) - 1):
+        array.append((J[i] - J[i + 1]) / (J[i - 1] - J[i]))
+        if array[i - 1] < MIN:
+            MIN = array[i - 1]
+    print("k optimal:", array.index(MIN) + 2)
+
+
+if __name__ == '__main__':
+    koptimal_with_kmeans()
+    plt.plot(range(1, 10), J)
+    plt.xlabel('Cluster')
+    plt.ylabel('J')
     plt.show()
