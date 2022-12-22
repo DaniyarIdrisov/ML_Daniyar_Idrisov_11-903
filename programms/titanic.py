6fa06d6f35c56d65e1a5af9a4f50de23cf363ece
Daniyar Idrisov <dan.id2001@mail.ru>
2022-12-22 22:27:13 +0300

    added new titanic.py and added openCSV

--- programms/titanic.py
+++ programms/titanic.py
@@ -1,23 +1,23 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 
-def start():
-    labels = []
-    result_pass = []
-    data = pd.read_csv("titanic.csv")
-    classes = data.Pclass
-    all_classes = data['Pclass'].unique()
-    results = [(i, classes[classes == i].count()) for i in all_classes]
-    for result in results:
-        labels.append(result[0])
-        result_pass.append(result[1])
-    fig, ax = plt.subplots()
-    ax.pie(result_pass, labels=labels, autopct='%1.1f%%',
-           shadow=True, startangle=90)
-    ax.axis('equal')
-    plt.show()
+def read_csv(filename):
+    data = pd.read_csv(filename)
+    return data
 
 
 if __name__ == '__main__':
-    start()
+    dataset = read_csv("titanic.csv")
+    print(dataset)
+    dataset.head()
+    g = sns.FacetGrid(dataset, height=6)
+    g.map(plt.hist, 'Age', bins=20)
+    plt.figure(figsize=(9, 6))
+    colors = sns.color_palette('pastel')
+    plt.pie(dataset.Pclass.value_counts(), labels=['Pclass: 3', 'Pclass: 1', 'Pclass: 2'], colors=colors,
+            autopct='%.2f%%')
+    plt.title('Распределение по классам')
+    plt.show()
+    sns.catplot(x='Pclass', y='Survived', hue='Sex', data=dataset, kind='bar')
